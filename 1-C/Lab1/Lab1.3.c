7f22d135d956b27794a8b3f6887fc2e7c467452a
Korayem22 <m.korayem1@gmail.com>
2025-02-04 14:19:46 +0200

    ..

--- 1-C/Lab1/Lab1.3.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    int num;
-    printf("please enter an integer ");
-    scanf("%d",&num);
-    printf("%d in octal is: %o and in hexadecimal is: %x",num,num,num);
-}
